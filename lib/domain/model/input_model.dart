79429fae477e0a5de95e59c69f0b802aec825bbb
oluwasegun <ilorioluwasegun@gmail.com>
2024-03-22 16:08:32 -0400

    Add test dependencies

---
+++ lib/domain/model/input_model.dart
@@ -0,0 +1,8 @@
+class InputModel {
+  final String first;
+  final String operator;
+  final String second;
+
+  InputModel(
+      {required this.first, required this.operator, required this.second});
+}
---
+++ lib/domain/validate_input_usecase.dart
@@ -0,0 +1,5 @@
+import 'package:smart_calculator/domain/model/input_model.dart';
+
+class ValidateInputUseCase {
+  call(InputModel params) {}
+}
