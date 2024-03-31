ac6266eeb71b38f2b75e6daaa2613a4073aa7756
oluwasegun <ilorioluwasegun@gmail.com>
2024-03-31 18:59:52 -0400

    Add dependency injection

--- lib/core/injection_container.dart
+++ lib/core/injection_container.dart
@@ -1,8 +1,16 @@
 import 'package:get_it/get_it.dart';
+import 'package:smart_calculator/domain/usecase/calculate_answer_usecase.dart';
+import 'package:smart_calculator/domain/usecase/validate_input_usecase.dart';
 import 'package:smart_calculator/presentation/calculator/input_calculator_bloc.dart';
 
 final di = GetIt.instance;
 
-// Future<void> init() async {
-//   di.registerFactory<InputCalculatorBloc>(() =>  )
-// }
+Future<void> init() async {
+  //blocs
+  di.registerFactory<InputCalculatorBloc>(() => InputCalculatorBloc(di.call()));
+
+  //Usecase
+  di.registerFactory<CalculateAnswerUseCase>(
+      () => CalculateAnswerUseCase(validateInputUseCase: di.call()));
+  di.registerFactory<ValidateInputUseCase>(() => ValidateInputUseCase());
+}
