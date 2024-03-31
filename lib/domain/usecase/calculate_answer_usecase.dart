722206b716d2cbffb317dba372a7e55029ca112f
oluwasegun <ilorioluwasegun@gmail.com>
2024-03-30 20:33:01 -0400

    Implement Division

--- lib/domain/usecase/calculate_answer_usecase.dart
+++ lib/domain/usecase/calculate_answer_usecase.dart
@@ -1,14 +1,40 @@
 import 'package:smart_calculator/domain/model/input_model.dart';
 import 'package:smart_calculator/domain/usecase/validate_input_usecase.dart';
 
+const nan = "Nan";
+
 class CalculateAnswerUseCase {
   final ValidateInputUseCase validateInputUseCase;
   CalculateAnswerUseCase({required this.validateInputUseCase});
-  double? call(InputModel data) {
+  String? call(InputModel data) {
     if (validateInputUseCase(data)) {
-      return 0.0;
+      return _calculateAnswer(data);
     } else {
       return null;
     }
+  }
+
+  String? _calculateAnswer(InputModel data) {
+    var secondNumber = num.parse(data.second);
+    if (secondNumber == 0) {
+      if (data.operator == "/" && secondNumber.toInt() == 0) {
+        return nan;
+      }
+    }
+    var firstNumber = num.parse(data.first);
+    if (data.firstNoSign == "-") {
+      firstNumber = 0 - firstNumber;
+    }
+    num answer = (firstNumber / secondNumber);
+    if (_isDecimal(answer)) {
+      return answer.toString();
+    } else {
+      return answer.toInt().toString();
+    }
+  }
+
+  bool _isDecimal(num number) {
+    // Check if the number is not an integer
+    return number % 1 != 0;
   }
 }
--- test/domain/calculate_answer_usecase_test.dart
+++ test/domain/calculate_answer_usecase_test.dart
@@ -11,17 +11,48 @@ import 'calculate_answer_usecase_test.mocks.dart';
 void main() {
   final mockValidateInputUseCase = MockValidateInputUseCase();
   CalculateAnswerUseCase testSubject;
+  testSubject =
+      CalculateAnswerUseCase(validateInputUseCase: mockValidateInputUseCase);
 
-  group("Answer calculator test", () {
+  group("Answer calculator test invalid inpur", () {
     setUp(() {
       when(mockValidateInputUseCase.call(any))
           .thenAnswer((realInvocation) => false);
     });
-    testSubject =
-        CalculateAnswerUseCase(validateInputUseCase: mockValidateInputUseCase);
+
     test("should return null with invalid input", () {
       var result = testSubject.call(const InputModel(firstNoSign: ""));
       expect(null, result);
+    });
+  });
+  group("Answer calculator test valid input", () {
+    setUp(() {
+      when(mockValidateInputUseCase.call(any))
+          .thenAnswer((realInvocation) => true);
+    });
+    test("should return Nan if second number is zero and operation is division",
+        () {
+      var result = testSubject.call(const InputModel(
+          firstNoSign: "-", first: "2", second: "0", operator: "/"));
+      expect(nan, result);
+    });
+
+    test("should return correct integer answer when operation is division", () {
+      var result = testSubject
+          .call(const InputModel(first: "100", second: "25", operator: "/"));
+      expect("4", result);
+    });
+
+    test("should return correct decimal answer when answer is division", () {
+      var result = testSubject.call(const InputModel(
+          firstNoSign: "-", first: "4", second: "8", operator: "/"));
+      expect("-0.5", result);
+    });
+
+    test("should return correct  answer when answer is division", () {
+      var result = testSubject.call(const InputModel(
+          firstNoSign: "-", first: "0", second: "8", operator: "/"));
+      expect("0", result);
     });
   });
 }
